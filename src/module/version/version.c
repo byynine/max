c358069396ed115d50d7a086f86b7eee27fee992
byynine <nutitota@tutanota.com>
2026-01-06 13:41:01 +0400

    update version

--- src/module/version/version.c
+++ src/module/version/version.c
@@ -1,1 +1,1 @@
-const char *version = "max 1.0.2d\nThis is a development version, do not expect complete, bugless features and ease of use.";
+const char *version = "max 1.0.3d\nThis is a development version, do not expect complete, bugless features and ease of use.";
