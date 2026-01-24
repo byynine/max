07d83d2d935fe79f5b26a488b3b4639aeeaaff53
byynine <nutitota@tutanota.com>
2026-01-24 14:02:48 +0400

    implementing tokenizer

--- src/main.c
+++ src/main.c
@@ -10,36 +10,72 @@ int process_line(char *lbuf, FILE *file)
     return strlen(lbuf);
 }
 
-int iskey(char *buf, char *key)
+char (*tokenize(char *str, const char delim))[64]
 {
-    if (!buf || !key) return 0;
+    if (!str) return NULL;
 
-    size_t blen = strlen(buf);
+    size_t max_toks = 32;
+    size_t tok_size = 64;
+    size_t toksi = 0;
+
+    char (*toks)[tok_size] = malloc(max_toks * tok_size);
+    if (!toks) return NULL;
+
+    char buf[2048];
+    size_t bufi = 0;
+
+    size_t slen = strlen(str);
+    if (slen <= 0) return NULL;
+
+    for (size_t i = 0; i < slen; i++)
+    {
+        if (bufi >= sizeof(buf) - 1) {
+            printf("bufi");
+            break;
+        }
+        if (str[i] == delim)
+        {
+            if (toksi >= max_toks) {
+                printf("bufi");
+                break;
+            }
+
+            buf[bufi] = '\0';
+
+            strncpy(toks[toksi], buf, sizeof(toks[toksi]) - 1);
+            toks[toksi][sizeof(toks[toksi]) - 1] = '\0';
+            toksi++;
+
+            bufi = 0;
+
+            continue;
+        }
+        buf[bufi++] = str[i];
+    }
+    if (bufi >= sizeof(buf) - 1 || toksi >= max_toks) return NULL;
+
+    buf[bufi] = '\0';
+
+    strncpy(toks[toksi], buf, sizeof(toks[toksi]) - 1);
+    toks[toksi][sizeof(toks[toksi]) - 1] = '\0';
+
+    bufi = 0;
+
+    return toks;
+}
+
+int is_key(char *str, char *key)
+{
+    if (!str || !key) return -1;
+
+    size_t slen = strlen(str);
     size_t klen = strlen(key);
 
-    char *nbuf = malloc(1024);
-    if (!nbuf) return 0;
-
-    size_t nbufi = 0;
-
-    for (size_t i = 0; i < blen; i++)
-    {
-        char c = buf[i];
-        for (size_t j = 0; j < klen; j++)
-        {
-            if (nbufi >= klen) break;
-            if (c == key[j]) nbuf[nbufi++] = key[j];
-        }
-    }
-
-    nbuf[nbufi] = '\0';
-
-    if (!strcmp(nbuf, key)) return 1;
-
-    free(nbuf);
+    if (klen > slen) { return -2; }
 
     return 0;
 }
+
 
 int main()
 {
@@ -49,13 +85,19 @@ int main()
     char lbuf[1024];
     int li = 1;
 
+    char (*toks)[64] = tokenize("hhellohelllohellohellohllohellohellohllohellohellohllohellohellohllohellohellohllohellohellohlohellohellohellohellohellohellohellohellohellohellohelloello bgege cwe", ' ');
+
+    printf("%s\n", toks[0]);
+    printf("%s\n", toks[1]);
+    printf("%s\n", toks[2]);
+
+    free(toks);
+
     while (fgets(lbuf, sizeof(lbuf), maxfile))
     {
         int len = process_line(lbuf, maxfile);
-        if (len == -1) { printf("line too long\n"); return 1; }
+        if (len == -1) { printf("line %d: line too long\n", li); return 1; }
 
-        if (iskey(lbuf, "ref "))
-            printf("ref key on line %d\n", li);
 
         li++;
     }
